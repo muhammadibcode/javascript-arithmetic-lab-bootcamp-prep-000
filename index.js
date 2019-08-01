1c68c3326551a0dafd8194cc458e6a463c8b9038
Muhammad Bhatti <muhammad.i.b@outlook.com>
2019-08-01 20:54:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ function divide(number1, number2) {
 }
 
 function inc(n) {
-  n += 1
+  return n += 1
 }
 
 function dec(n) {
-  n -= 1
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
